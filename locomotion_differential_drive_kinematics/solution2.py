f21880a926472dc40231f502f690353b1ec69fd8
ibkvictor <eze99victor@gmail.com>
2022-02-23 12:30:00 +0100

    locomotion task

--- locomotion_differential_drive_kinematics/solution2.py
+++ locomotion_differential_drive_kinematics/solution2.py
@@ -1,17 +1,28 @@
 # author: Victor Ezekiel, github.com/ibkvictor
 import math
+import numpy as np
 
 # --- (a)
 def diffdrive(x, y, theta, v_l, v_r, t, l):
-	rot = lambda w, t : np.array([[cos(w * t), -sin(w * t), 0], [math.sin(w * t), math.cos(w * t), 0], [0, 0, 1]])
-	position = np.array([x, y, theta]).T
+	# circular motion
+	if(v_r == v_l):
+		x_n = x + 1/2 * (v_l + v_r) * math.cos(theta)
 
-	R = 0.5 * (l * (v_l + v_r)/(v_r - v_l))
-	w = (v_r - v_l)/l
-	v = (v_r + v_l)/2
-	ICC = [(x - R * math.sin(theta)), (y + R * cos(theta))]
+		y_n = y + 1/2 * (v_l + v_r) * math.sin(theta)
+		return [x_n, y_n, theta]
+	#circular motion
+	else:
+		rot = lambda w, t : np.array([[math.cos(w * t), -math.sin(w * t), 0], [math.sin(w * t), math.cos(w * t), 0], [0, 0, 1]])
+		position = np.array([x, y, theta]).T
+
+		R = 0.5 * (l * (v_l + v_r)/(v_r - v_l))
+		w = (v_r - v_l)/l
+		v = (v_r + v_l)/2
+		ICC = [(x - R * math.sin(theta)), (y + R * math.cos(theta))]
 	
-	return list(np.matmul(rot(w, t), position - np.array(ICC + [0]).T) + np.array(ICC + [w * t]).T
+		return (list(np.matmul(rot(w, t), position - np.array(ICC + [0]).T) + np.array(ICC + [w * t]).T))
+
+
 # --- (b)
 
 def control():
