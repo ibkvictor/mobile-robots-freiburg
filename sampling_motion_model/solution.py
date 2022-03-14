eb7c6b3020e9c64b6b134aa4542328125e0f01da
ibkvictor <eze99victor@gmail.com>
2022-03-14 09:47:19 +0100

    sampling motion model task

---
+++ sampling_motion_model/solution.py
@@ -0,0 +1,24 @@
+import numpy as np
+import math
+import timeit
+
+# Exercise 1
+def normal_a():
+	return sum(np.random.random(12)) / 2
+
+
+def normal_b():
+	y = 
+	if math.sin(x) < y:
+		n
+	return
+
+def normal_c():
+	u1 = np.random.random()
+	u2 = np.random.random()
+	return math.cos(2 * math.pi * u1) * math.sqrt(-1 * math.log(u2))
+
+print(timeit.timeit(normal_a()))
+print(timeit.timeit(normal_b()))
+print(timeit.timeit(normal_c()))
+
